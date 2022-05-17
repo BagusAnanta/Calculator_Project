f549c2e02298cb38fa1cb6244c438ff035863afb
BagusAnanta <bagusananta278@gmail.com>
2022-05-17 07:06:50 +0700

    Calculator kotlin project

--- app/src/main/java/com/bsoftwarefoundation/calculator/Calculator_activity.kt
+++ app/src/main/java/com/bsoftwarefoundation/calculator/Calculator_activity.kt
@@ -2,10 +2,15 @@ package com.bsoftwarefoundation.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class Calculator_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        // set content
         setContentView(R.layout.activity_main)
+
     }
 }
