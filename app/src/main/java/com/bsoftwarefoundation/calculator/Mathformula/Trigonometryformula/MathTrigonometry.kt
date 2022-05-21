1489a2badc60b1c51759b9ccf20aa6825dd45d36
BagusAnanta <bagusananta278@gmail.com>
2022-05-21 19:24:20 +0700

    Calculator kotlin project

--- app/src/main/java/com/bsoftwarefoundation/calculator/Mathformula/Trigonometryformula/MathTrigonometry.kt
+++ app/src/main/java/com/bsoftwarefoundation/calculator/Mathformula/Trigonometryformula/MathTrigonometry.kt
@@ -2,18 +2,44 @@ package com.bsoftwarefoundation.calculator.Mathformula.Trigonometryformula
 
 class MathTrigonometry {
 
-    fun Sinus(X : Double) : Double{
+    // Trigonometry on Radiant
+    fun SinusRadiant(X : Double) : Double{
         val result = Math.sin(X)
-        return result
+        val radian_result = Math.toRadians(result)
+        return radian_result
     }
 
-    fun Cosinus(X : Double) : Double{
+    fun CosinusRadiant(X : Double) : Double{
         val result = Math.cos(X)
-        return result
+        val radian_result = Math.toRadians(result)
+        return radian_result
     }
 
-    fun Tangen(X : Double) : Double{
+    fun TangenRadiant(X : Double) : Double{
         val result = Math.tan(X)
-        return result
+        val radian_result = Math.toRadians(result)
+        return radian_result
+    }
+
+    inner class MathTrigonometryDegress{
+
+        // Trigonometry on Degress
+        fun SinusDegress(X : Double) : Double{
+            val result = Math.sin(X)
+            val degress_result = Math.toDegrees(result)
+            return degress_result
+        }
+
+        fun CosinusDegress(X : Double) : Double{
+            val result = Math.cos(X)
+            val degress_result = Math.toDegrees(result)
+            return degress_result
+        }
+
+        fun TangenDegress(X : Double) : Double{
+            val result = Math.tan(X)
+            val degress_result = Math.toDegrees(result)
+            return degress_result
+        }
     }
 }
--- app/src/main/java/com/bsoftwarefoundation/calculator/Mathformula/Trigonometryformula/MathTrigonometryInverse.kt
+++ app/src/main/java/com/bsoftwarefoundation/calculator/Mathformula/Trigonometryformula/MathTrigonometryInverse.kt
@@ -1,16 +1,47 @@
 package com.bsoftwarefoundation.calculator.Mathformula.Trigonometryformula
 
 class MathTrigonometryInverse {
+    // Inverse formula in trigonometry sin^-1,cos^-1 ,tan^-1 and inverse have 2 in radiant or degress
 
-    fun SinusInverse(){
+    fun SinusInverseRadiant(X : Double) : Double{
+        val result = Math.asin(X)
+        val radian_result = Math.toRadians(result)
+        return radian_result
+    }
+
+    fun CosinusInverseRadiant(X : Double) : Double{
+        val result = Math.acos(X)
+        val radian_result = Math.toRadians(result)
+        return radian_result
+    }
+
+    fun TangeInverseRadiant(X : Double) : Double{
+        val result = Math.atan(X)
+        val radian_result = Math.toRadians(result)
+        return radian_result
+    }
+
+    inner class MathTrigonometryInverseDegress{
+
+        fun SinusInverseDegress(X : Double) : Double{
+            val result = Math.asin(X)
+            val degress_result = Math.toDegrees(result)
+            return degress_result
+        }
+
+        fun CosinusInverseDegress(X : Double) : Double{
+            val result = Math.acos(X)
+            val degress_result = Math.toDegrees(result)
+            return degress_result
+        }
+
+        fun TangenInverseDegress(X : Double) : Double{
+            val result = Math.atan(X)
+            val degress_result = Math.toDegrees(result)
+            return degress_result
+        }
 
     }
 
-    fun CosinusInverse(){
 
-    }
-
-    fun TangeInverse(){
-
-    }
 }
