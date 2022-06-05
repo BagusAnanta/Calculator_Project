1cfcde995936f06f58f71b6373fd0949cd0c129a
BagusAnanta <bagusananta278@gmail.com>
2022-06-05 17:38:26 +0700

    Calculator kotlin project

--- app/src/main/java/com/bsoftwarefoundation/calculator/Mathformula/Trigonometryformula/MathTrigonometryInverse.kt
+++ app/src/main/java/com/bsoftwarefoundation/calculator/Mathformula/Trigonometryformula/MathTrigonometryInverse.kt
@@ -4,37 +4,37 @@ class MathTrigonometryInverse {
     // Inverse formula in trigonometry sin^-1,cos^-1 ,tan^-1 and inverse have 2 in radiant or degress
 
     fun SinusInverseRadiant(X : Double) : Double{
-        val result = Math.asin(X)
+        val result = Math.toRadians(Math.asin(Math.sin(X)))
         return result
     }
 
     fun CosinusInverseRadiant(X : Double) : Double{
-        val result = Math.acos(X)
+        val result = Math.toRadians(Math.acos(Math.cos(X)))
         return result
     }
 
     fun TangeInverseRadiant(X : Double) : Double{
-        val result = Math.atan(X)
+        val result = Math.toRadians(Math.atan(Math.tan(X)))
         return result
     }
 
     inner class MathTrigonometryInverseDegress{
 
         fun SinusInverseDegress(X : Double) : Double{
-            val convert_radiant = Math.toDegrees(X)
-            val result = Math.asin(convert_radiant)
+            val convert_radiant = Math.toRadians(X)
+            val result = Math.toDegrees(Math.asin(Math.sin(convert_radiant)))
             return result
         }
 
         fun CosinusInverseDegress(X : Double) : Double{
-            val convert_radiant = Math.toDegrees(X)
-            val result = Math.acos(convert_radiant)
+            val convert_radiant = Math.toRadians(X)
+            val result = Math.toDegrees(Math.asin(Math.cos(convert_radiant)))
             return result
         }
 
         fun TangenInverseDegress(X : Double) : Double{
-            val convert_radiant = Math.toDegrees(X)
-            val result = Math.atan(convert_radiant)
+            val convert_radiant = Math.toRadians(X)
+            val result = Math.toDegrees(Math.atan(Math.tan(convert_radiant)))
             return result
         }
 
